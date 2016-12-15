cc2383fccb54ed71f5dd3c50e8ca1029e4ef9844
JuliaAf <a290288@gmail.com>
2016-12-16 01:55:26 +0300

    Index:adaptive, animations

--- js/common.js
+++ js/common.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
- $(".projects__nav-toggle.active").after("<div class='projects__nav-line'></div>");
 var toggle = $(".projects__nav-toggle");
   toggle.click(function(){
   var that = $(this),
@@ -9,78 +8,68 @@ var toggle = $(".projects__nav-toggle");
     toggle.removeClass("active");
     that.addClass("active");
 
-    $(".projects__nav-line").remove();
-    that.after("<div class='projects__nav-line'></div>");
-    // $(".slider-toggle:last-child + .slider-line").remove();
-    // $(".slider-item:last-child + .slider-line").remove();
-    // $(".slider-toggle:last-child").next(".slider-line").remove();
-    //
     item.removeClass("active");
     item.eq(togglePosition).addClass("active");
   });
 
+/*wow*/
+wow = new WOW(
+    {
+    boxClass:     'wow',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  )
+  wow.init();
 
   /*owl-carousel*/
-
   $(".index-carousel").owlCarousel({
 
-    // Most important owl features
     items : 1,
     singleItem : true,
     itemsScaleUp : false,
 
-    //Basic Speeds
     slideSpeed : 200,
     paginationSpeed : 800,
     rewindSpeed : 1000,
 
-    //Autoplay
     autoPlay : true,
     stopOnHover : false,
 
-    // Navigation
     navigation : false,
     navigationText : ["prev","next"],
     rewindNav : true,
     scrollPerPage : false,
 
-    //Pagination
     pagination : true,
     paginationNumbers: false,
 
-    // Responsive
     responsive: true,
     responsiveRefreshRate : 200,
     responsiveBaseWidth: window,
 
-    // CSS Styles
     baseClass : "owl-carousel",
     theme : "owl-theme",
 
-    //Lazy load
     lazyLoad : false,
     lazyFollow : true,
     lazyEffect : "fade",
 
-    //Auto height
     autoHeight : false,
 
-    //JSON
     jsonPath : false,
     jsonSuccess : false,
 
-    //Mouse Events
     dragBeforeAnimFinish : true,
     mouseDrag : true,
     touchDrag : true,
 
-    //Transitions
     transitionStyle : false,
 
-    // Other
     addClassActive : false,
 
-    //Callbacks
     beforeUpdate : false,
     afterUpdate : false,
     beforeInit: false,
@@ -91,6 +80,11 @@ var toggle = $(".projects__nav-toggle");
     startDragging : false,
     afterLazyLoad : false
 
+});
+
+/*mobile nav*/
+$(".header__open-toggle").click(function(){
+  $(".header__nav").slideToggle();
 })
 
 });//ready-end
