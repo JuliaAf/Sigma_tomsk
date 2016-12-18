f466775d2f22573deec5ab6a2a334147e7c623c6
JuliaAf <a290288@gmail.com>
2016-12-19 00:19:13 +0300

    changes 16.12.16

--- js/common.js
+++ js/common.js
@@ -1,28 +1,41 @@
 $(document).ready(function() {
-var toggle = $(".projects__nav-toggle");
-  toggle.click(function(){
-  var that = $(this),
-      togglePosition = that.index(),
-      item = $(".projects__item");
+  var toggle = $(".projects__nav-toggle");
+    toggle.click(function(){
+    var that = $(this),
+        togglePosition = that.index(),
+        item = $(".projects__item");
 
-    toggle.removeClass("active");
-    that.addClass("active");
+      toggle.removeClass("active");
+      that.addClass("active");
 
-    item.removeClass("active");
-    item.eq(togglePosition).addClass("active");
+      item.removeClass("active");
+      item.eq(togglePosition).addClass("active");
+    });
+
+  /*sticky header*/
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 75){
+      $(".header").addClass("header--sticky");
+    }
+    else{
+      $(".header").removeClass("header--sticky");
+    };
   });
 
-/*wow*/
-wow = new WOW(
-    {
-    boxClass:     'wow',      // default
-    animateClass: 'animated', // default
-    offset:       0,          // default
-    mobile:       true,       // default
-    live:         true        // default
-  }
-  )
-  wow.init();
+  /*wow*/
+  wow = new WOW(
+      {
+      boxClass:     'wow',      // default
+      animateClass: 'animated', // default
+      offset:       0,          // default
+      mobile:       true,       // default
+      live:         true        // default
+    }
+    )
+    wow.init();
+
+
+  $(".aims::after").addClass("wow slideInLeft");
 
   /*owl-carousel*/
   $(".index-carousel").owlCarousel({
@@ -80,11 +93,11 @@ wow = new WOW(
     startDragging : false,
     afterLazyLoad : false
 
-});
+  });
 
-/*mobile nav*/
-$(".header__open-toggle").click(function(){
-  $(".header__nav").slideToggle();
-})
+  /*mobile nav*/
+  $(".header__open-toggle").click(function(){
+    $(".header__nav").slideToggle();
+  })
 
 });//ready-end
